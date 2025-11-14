4ac4172a45c80ad88834f2289126a39640650445
Jmarchionno <giuseppelaurenmarchionno@gmail.com>
2025-11-14 13:03:50 -0500

    plot data

---
+++ code/ANOVA_sediment.R
@@ -0,0 +1,9 @@
+setwd("/Users/joe/Desktop/R_projects/CH3_Patchconfig/patchconfiguration_effects_env")
+
+###Analysis of variance
+##is there a correlation between treatment type and TC,TIC,TN
+
+library(tidyverse)
+library(ggplot2)
+
+dat<- 
