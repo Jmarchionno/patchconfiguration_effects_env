8e1f791671aa7c4df0b38ec238f739cc4543935a
Jmarchionno <giuseppelaurenmarchionno@gmail.com>
2026-07-16 10:18:21 -0400

    run PERMANOVA. No sig location or dispersion effects

--- code/Model_sediment.R
+++ code/Model_sediment.R
@@ -148,8 +148,55 @@ ggplot(oyster_dat, aes(x=treatment,y=percent.cover)) +
 perm_tex_dat<-tex_dat[, c(9,11,13,16)]
 
 #Run NMDS Model for Visualizing the composition
-
+set.seed(123)
 nmds_result<-metaMDS (perm_tex_dat, distance = "bray")
 
 #Extract NMDS Scores 
 nmds_scores <-as.data.frame(scores(nmds_result)$sites)
+
+#Find out the centroids
+
+group_centroids <- data.frame(
+  Treatment = c("h", "l","c"),
+  Centroid_X = c(mean(nmds_scores$NMDS1[tex_dat$Treatment == "h"]),
+                mean(nmds_scores$NMDS1[tex_dat$Treatment == "l"]),
+                 mean(nmds_scores$NMDS1[tex_dat$Treatment == "c"])),
+  
+  Centroid_Y = c(mean(nmds_scores$NMDS2[tex_dat$Treatment == "h"]),
+                 mean(nmds_scores$NMDS2[tex_dat$Treatment == "l"]),
+                 mean(nmds_scores$NMDS2[tex_dat$Treatment == "c"])))
+
+###Create data frame for ggplot
+
+#plot_NMDS_tex_data<-data.frame(Treatment = tex_dat$Treatment,
+  #NMDS1=nmds_scores$NMDS1,
+  #NMDS2=nmds_scores$NMDS2,
+  #xend=c(rep(group_centroids[1,2],10),rep(group_centroids[2,2],10)),
+  #yend=c(rep(group_centroids[1,3],10),rep( group_centroids[2,3],10)))
+
+plot_NMDS_tex_data <- data.frame(
+  Treatment = tex_dat$Treatment,
+  NMDS1 = nmds_scores$NMDS1,
+  NMDS2 = nmds_scores$NMDS2
+)
+
+plot_NMDS_tex_data <- merge(
+  plot_NMDS_tex_data,
+  group_centroids,
+  by = "Treatment"
+)
+
+names(plot_NMDS_tex_data)[4:5] <- c("xend", "yend")
+
+#Plot the data
+
+ggplot(plot_NMDS_tex_data, aes(NMDS1,NMDS2)) + 
+  geom_point(aes(color = Treatment),size=2)+ 
+  stat_ellipse(geom = "polygon", alpha = 0.04, aes(group = Treatment), 
+               color = "black",fill="blue")+ 
+  geom_point(data = group_centroids, aes(x = Centroid_X, y = Centroid_Y), 
+             color = "black", size = 2, shape = 7)+
+  geom_segment(data = plot_NMDS_tex_data, aes(x =NMDS1, y = NMDS2, 
+                                     xend = xend, yend = yend, color = Treatment), alpha = 0.5)+
+  scale_color_manual(name= "Treatment",labels= unique(plot_NMDS_tex_data$Treatment),
+                     values= c("darkolivegreen", "darkviolet","darkorange1"))+ theme_bw()
